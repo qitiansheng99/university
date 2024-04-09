f5646ec5a66f846a7f37ba6b350e0915a7da7e31
qitiansheng99 <149516166+qitiansheng99@users.noreply.github.com>
2024-04-09 18:09:35 +0800

    Add files via upload

---
+++ 消失的数字/消失的数字/消失的数字.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i, n;
+	int num = 0;
+	scanf("%d", &n);
+	int* arr = (int*)malloc(sizeof(int) * n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+		num ^= arr[i];
+	}
+	for (i = 0; i < n + 1; i++)
+	{
+		num ^= i;
+	}
+	printf("%d\n", num);
+	return 0;
+}
