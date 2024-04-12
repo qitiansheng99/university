b852cc9f2ad5c454c1018f0763c1d0d70ab8da69
qitiansheng99 <149516166+qitiansheng99@users.noreply.github.com>
2024-04-12 23:06:21 +0800

    Add files via upload

---
+++ 力扣_删除有序数组中的重复项/力扣_删除有序数组中的重复项/力扣_删除有序数组中的重复项.c
@@ -0,0 +1,52 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdlib.h>
+#include<stdio.h>
+int removeDuplicates(int* nums, int numsSize)
+{
+	if (numsSize == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		int dst = 0, begin = 0, end = 1;
+		while (end <= numsSize)
+		{
+			if (nums[begin] == nums[end])
+			{
+				if (end == numsSize)
+				{
+					nums[dst] = nums[begin];
+					break;
+				}
+				end++;
+			}
+			else
+			{
+				nums[dst] = nums[begin];
+				if (end < numsSize)
+				{
+					dst++;
+				}
+				begin = end++;
+			}
+		}
+		return dst + 1;
+	}
+}
+int main()
+{
+	int size;
+	scanf("%d", &size);
+	int* nums = (int*)malloc(sizeof(int) * size);
+	for (int i = 0; i < size; i++)
+	{
+		scanf("%d", &nums[i]);
+	}
+	int n = removeDuplicates(nums, size);
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ", nums[i]);
+	}
+	return 0;
+}
