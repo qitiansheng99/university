d0d303902e3aeff7a933f2242e103547a26d6338
qitiansheng99 <149516166+qitiansheng99@users.noreply.github.com>
2024-04-14 23:17:47 +0800

    Add files via upload

---
+++ 蓝桥杯_特殊日期/蓝桥杯_特殊日期/蓝桥杯_特殊日期.c
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    // 请在此输入您的代码
+    int month1[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+    int month2[13] = { 0,31,29,31,30,31,30,31,31,30,31,30,31 };
+    int day, month, year;
+    int ans = 0;
+    for (year = 2000; year <= 1999999; year++)
+    {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        {
+            for (month = 1; month <= 12; month++)
+            {
+                for (day = 1; day <= month2[month]; day++)
+                {
+                    if (year % month == 0 && year % day == 0)
+                    {
+                        ans++;
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (month = 1; month <= 12; month++)
+            {
+                for (day = 1; day <= month1[month]; day++)
+                {
+                    if (year % month == 0 && year % day == 0)
+                    {
+                        ans++;
+                    }
+                }
+            }
+        }
+    }
+    printf("%d", ans + 1);
+    return 0;
+}
