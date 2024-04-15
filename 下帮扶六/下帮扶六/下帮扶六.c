8b4a8b856a56dd7452cd3e6e1351de255ba8d67d
qitiansheng99 <149516166+qitiansheng99@users.noreply.github.com>
2024-04-15 23:24:19 +0800

    Add files via upload

---
+++ 下帮扶六/下帮扶六/下帮扶六.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n, i;
+	int ans = 0, sum = 0;
+	scanf("%d", &n);
+	int* arr = (int*)malloc(sizeof(int) * n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+		sum += arr[i];
+	}
+	int ave = sum / n;
+	for (i = 0; i < n ; i++)
+	{
+		arr[i] = arr[i] - ave;
+		if (arr[i] == 0)
+		{
+			continue;
+		}
+		else
+		{
+			arr[i + 1] += arr[i];
+			ans++;
+		}
+	}
+	printf("%d\n", ans);
+	return 0;
+}
