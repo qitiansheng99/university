b1d2b25c6126176c4c0581e48707d4e4c542e600
qitiansheng99 <149516166+qitiansheng99@users.noreply.github.com>
2024-04-23 23:39:30 +0800

    Add files via upload

---
+++ 力扣_翻转链表/力扣_翻转链表/力扣_翻转链表.c
@@ -0,0 +1,59 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct SLTNode
+{
+	int data;
+	struct SLTNode* next;
+}SLTNode;
+
+SLTNode* SLT_turn(SLTNode* head)
+{
+	SLTNode* n1, * n2, * n3;
+	if (head == NULL)
+	{
+		return NULL;
+	}
+	n1 = NULL;
+	n2 = head;
+	n3 = head->next;
+	while (n2 != 0)
+	{
+		n2->next = n1;
+		n1 = n2;
+		n2 = n3;
+		if (n3 != NULL)
+		{
+			n3 = n3->next;
+		}
+	}
+	return n1;
+}
+
+int main()
+{
+	SLTNode* plist = NULL;
+	SLTNode* n1 = (SLTNode*)malloc(sizeof(SLTNode));
+	SLTNode* n2 = (SLTNode*)malloc(sizeof(SLTNode));
+	SLTNode* n3 = (SLTNode*)malloc(sizeof(SLTNode));
+	SLTNode* n4 = (SLTNode*)malloc(sizeof(SLTNode));
+	SLTNode* n5 = (SLTNode*)malloc(sizeof(SLTNode));
+	n1->data = 1;
+	n2->data = 2;
+	//n3->data = 3;
+	//n4->data = 4;
+	//n5->data = 5;
+	plist = n1;
+	n1->next = n2;
+	n2->next = NULL;
+	/*n3->next = n4;
+	n4->next = n5;
+	n5->next = NULL;*/
+	plist = SLT_turn(plist);
+	while (plist != NULL)
+	{
+		printf("%d ", plist->data);
+		plist = plist->next;
+	}
+	return 0;
+}
